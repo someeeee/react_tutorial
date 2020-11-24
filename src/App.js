78f3792324cfa178dd808b8be2eaaf6fcfb7e910
somi <somidevelop@gmail.com>
2020-11-24 13:51:47 +0900

    feat: 컴포넌트 추가
    pure.html에 리액트 없이 짠 코드를 App.js에 리액트로 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import { Component } from 'react';
 import './App.css';
 
+class Subject extends Component {
+  render() {
+    return(
+      <header>
+            <h1>WEB</h1>
+            world wide WEB!
+        </header> 
+    );
+  }
+}
+
+class TOC extends Component {
+  render() {
+    return(
+      <nav>
+            <ul>
+                <li><a href="1.html">HTML</a></li>
+                <li><a href="2.html">CSS</a></li>
+                <li><a href="3.html">JavaScript</a></li>
+            </ul>
+        </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return(
+      <article>
+         <h2>HTML</h2>
+          HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
+// 유사 javascript (jsx?)
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hello, react!
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
     );
   }
