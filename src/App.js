9904a9c9dd151872d89a51cf9750321472650a52
somi <somidevelop@gmail.com>
2020-11-24 13:40:24 +0900

    refactor: App.js의 함수로 이루어진 App을 클래스 컴포넌트로 변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      hello, react!
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hello, react!
+      </div>
+    );
+  }
 }
 
 export default App;
